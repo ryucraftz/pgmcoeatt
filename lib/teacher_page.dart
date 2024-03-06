4e9d75d13217d6d33b88d87c205499e8f3409810
ryucraftz <vaibhavsathe020>
2024-03-07 04:28:49 +0530

    bottom dashboard added

--- lib/teacher_page.dart
+++ lib/teacher_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pgmcoe_att/login_page.dart';
 
 class TeacherPage extends StatefulWidget {
@@ -8,8 +9,23 @@ class TeacherPage extends StatefulWidget {
 }
 
 class _TeacherPageState extends State<TeacherPage> {
+  double screenHeight = 0;
+  double screenWeight = 0;
+
+  Color primary = const Color(0xFF507583);
+
+  int currentIndex = 0;
+
+  List<IconData> navigationIcons = [
+    FontAwesomeIcons.calendarAlt,
+    FontAwesomeIcons.check,
+    FontAwesomeIcons.userAlt,
+  ];
+
   @override
   Widget build(BuildContext context) {
+    screenWeight = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         title: Text('PGMCOE ATTENDANCE'),
@@ -27,12 +43,69 @@ class _TeacherPageState extends State<TeacherPage> {
           ),
         ],
       ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            // Add your logic for handling the button press here
-          },
-          child: Text('Classes'),
+      
+      bottomNavigationBar: Container(
+        height: 70,
+        margin: EdgeInsets.only(left: 12, right: 12, bottom: 24),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 10,
+              offset: Offset(2, 2),
+            )
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (int i = 0; i < navigationIcons.length; i++)
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        currentIndex = i;
+                      });
+                    },
+                    child: Container(
+                      height: screenHeight,
+                      width: screenWeight,
+                      color: Colors.white,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              navigationIcons[i],
+                              color:
+                                  i == currentIndex ? primary : Colors.black54,
+                              size: i == currentIndex ? 30 : 26,
+                            ),
+                            i == currentIndex
+                                ? Container(
+                                    margin: EdgeInsets.only(top: 6),
+                                    height: 3,
+                                    width: 22,
+                                    decoration: BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(40)),
+                                      color: primary,
+                                    ),
+                                  )
+                                : const SizedBox(),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
